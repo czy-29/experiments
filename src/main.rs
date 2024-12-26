6d92cdefc360e2af917de477025dc9ca03571c7e
29 <791603901@qq.com>
2024-12-26 21:12:18 +0800

    测试多Runtime

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::thread;
+use tokio::{runtime, time::{sleep, Duration}};
+
 fn main() {
-    println!("Hello, world!");
+    let handle = thread::spawn(rt2);
+    rt1();
+    handle.join().unwrap();
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn rt1() {
+    run().await;
+}
+
+#[tokio::main]
+async fn rt2() {
+    run().await;
+}
+
+async fn run() {
+    sleep_mini().await;
+    print_flavor();
+    sleep_mini().await;
+}
+
+async fn sleep_mini() {
+    sleep(Duration::from_secs_f64(0.5)).await;
+}
+
+fn print_flavor() {
+    println!("{:?}", runtime::Handle::current().runtime_flavor());
 }
